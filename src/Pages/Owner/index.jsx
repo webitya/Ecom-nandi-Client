09fda3ba0f36d96da6a3f4b26f75fba9dde8426b
utkarshwebitya <uks797978@gmail.com>
2024-12-02 11:18:54 +0530

    owner update

--- src/Pages/Owner/index.jsx
+++ src/Pages/Owner/index.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import LayoutEl from "../../Shared/LayoutEl"
 import OwnerDashBoardEl from "../../Components/OwnerComponent/OwnerDashboardEl"
-import OwnerPanditChain from "../../Components/OwnerComponent/PanditChain"
-import OwnerSellerChain from "../../Components/OwnerComponent/SellerChain"
+// import OwnerPanditChain from "../../Components/OwnerComponent/PanditChain"
+// import OwnerSellerChain from "../../Components/OwnerComponent/SellerChain"
 import OwnerPanditAll from "../../Components/OwnerComponent/OwnerPanditAll"
 import OwnerSellerAll from "../../Components/OwnerComponent/OwnerSellerAll"
 import SpecificPanditEl from "../../Components/OwnerComponent/SpecificPanditEl"
 import SpecificSellerEl from "../../Components/OwnerComponent/SpecificSellerEl"
+import RoleChngRequest from "../../Components/OwnerComponent/RoleChngRequest"
 
 const Owner = () => {
   return (
@@ -14,10 +15,11 @@ const Owner = () => {
       <LayoutEl>
         <Routes>
           <Route path="/" element={<OwnerDashBoardEl />} />
-          <Route path="/panditRequest" element={<OwnerPanditChain />} />
-          <Route path="/sellerRequest" element={<OwnerSellerChain />} />
+          {/* <Route path="/panditRequest" element={<OwnerPanditChain />} />
+          <Route path="/sellerRequest" element={<OwnerSellerChain />} /> */}
           <Route path="/pandits" element={<OwnerPanditAll />} />
           <Route path="/sellers" element={<OwnerSellerAll />} />
+          <Route path="/roleChangeRequest" element= {<RoleChngRequest />} />
           <Route path="/pandits/:id" element={<SpecificPanditEl />} />
           <Route path="/sellers/:id" element={<SpecificSellerEl />} />
         </Routes>
